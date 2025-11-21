1593b89fc966b6bdf485c632597985975fe232e8
sean <qaz1996001@gmail.com>
2025-11-21 20:33:43 +0800

    |-src |- pipelinecore |-- dicomseg

--- src/pipelinecore/dicomseg/utils/base.py
+++ src/pipelinecore/dicomseg/utils/base.py
@@ -5,16 +5,17 @@ import os
 import pathlib
 from typing import Any
 
+import SimpleITK as sitk
 import matplotlib.colors as mcolors
 import nibabel as nib
 import numpy as np
 import pydicom
-import pydicom_seg
-import SimpleITK as sitk
 from pydicom import FileDataset
 from pydicom.dicomdir import DicomDir
+import pydicom_seg
 
 EXAMPLE_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                            'resource',
                             'SEG_20230210_160056_635_S3.dcm')
 DCM_EXAMPLE = pydicom.dcmread(EXAMPLE_FILE)
 
