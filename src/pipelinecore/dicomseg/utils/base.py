1c4adcba9c40c68ede704f43ebe1567f8f4b540f
user <a03440@tmu.edu.tw>
2026-01-29 12:39:45 +0800

    fix path EXAMPLE_FILE

--- src/pipelinecore/dicomseg/utils/base.py
+++ src/pipelinecore/dicomseg/utils/base.py
@@ -16,7 +16,9 @@ from pydicom import FileDataset
 from pydicom.dicomdir import DicomDir
 
 EXAMPLE_FILE = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "SEG_20230210_160056_635_S3.dcm"
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+    "resource",
+    "SEG_20230210_160056_635_S3.dcm"
 )
 DCM_EXAMPLE = pydicom.dcmread(EXAMPLE_FILE)
 
